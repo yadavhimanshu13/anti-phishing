a706ee8d43b7383090a68a7010432e5fb6a6be25
Levan Velijanashvili <stichoza@gmail.com>
2015-01-17 20:29:08 +0400

    Scripts and CSS

---
+++ js/source.js
@@ -0,0 +1,21 @@
+jQuery(function($) {
+
+    'use strict';
+
+    $('.selectpicker').selectpicker();
+    
+    $(document).on('submit', '#form', function(event) {
+        event.preventDefault();
+        if (!confirm('Start spamming?'))
+            return;
+        alert('hey');
+    });
+
+    $(document).on('click', '#add-field', function(event) {
+        var methodName = $('#faker-box').val();
+        if (methodName == 'text') {
+            // WAT
+        }
+    });
+
+});
